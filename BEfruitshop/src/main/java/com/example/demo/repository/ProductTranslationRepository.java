de1d84f0b61724ca1bb70a8e1a9ae2262f19100d
Tanhandsome47 <118707511+Tanhandsome47@users.noreply.github.com>
2025-05-29 23:22:43 +0700

    Delete BEfruitshop/src/main/java/com/example/demo/repository/ProductTranslationRepository.java

--- BEfruitshop/src/main/java/com/example/demo/repository/ProductTranslationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.ProductTranslation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductTranslationRepository extends JpaRepository<ProductTranslation, Long> {
-    List<ProductTranslation> findByProductIdAndLanguage(Integer productId, String language);
-}
