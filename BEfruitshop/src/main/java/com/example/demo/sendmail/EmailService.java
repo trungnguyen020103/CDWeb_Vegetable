b55a13b9fa54b7aa0ab1331f23be3bb7ba1f273d
Tanhandsome47 <118707511+Tanhandsome47@users.noreply.github.com>
2025-05-29 23:23:03 +0700

    Delete BEfruitshop/src/main/java/com/example/demo/sendmail/EmailService.java

--- BEfruitshop/src/main/java/com/example/demo/sendmail/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.sendmail;
-
-import java.util.Locale;
-
-public interface EmailService {
-    String sendSimpleMail(EmailDetails details, Locale locale);
-    boolean checkCode(String code, String email);
-}
